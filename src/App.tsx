a1281bd21cc7df5e054e537be8a13c3d43a2a8d4
sk <seonk94@gmail.com>
2020-10-28 22:48:57 +0900

    feat: Create main template & SymbolTable shape

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import styled from 'styled-components';
 import Navbar from './components/Navbar';
-import { NavbarHeight } from './constants';
+import { BodyContainerPadding, NavbarHeight } from './constants';
+import MainTemplate from './template/main';
 
 const BodyContainer = styled.div`
 	display: block;
 	margin-top: ${NavbarHeight}px;
-	min-height: calc(100vh - ${NavbarHeight}px);
+	min-height: calc(100vh - ${NavbarHeight + BodyContainerPadding * 2}px);
 	height: 100%;
+	padding: ${BodyContainerPadding}px;
 `;
 
 function App() {
 	return (
-		<div className="App">
+		<div>
 			<Navbar />
-			<BodyContainer />
+			<BodyContainer>
+				<MainTemplate />
+			</BodyContainer>
 		</div>
 	);
 }
