2ad683b8f4aa0fe5850b0d99239f91deb6d8f7f1
sk <seonk94@gmail.com>
2020-10-30 23:14:22 +0900

    fix: Change Navbar position

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 import Navbar from './components/Navbar';
-import { NavbarHeight } from './constants';
 
 const BodyContainer = styled.div`
 	display: block;
-	margin-top: ${NavbarHeight}px;
-	min-height: calc(100vh - ${NavbarHeight}px);
 	height: 100%;
 `;
 
--- src/components/Navbar/css.ts
+++ src/components/Navbar/css.ts
@@ -2,11 +2,7 @@ import styled from 'styled-components';
 import { NavbarHeight } from '../../constants';
 
 const NavHeader = styled.header`
-	position: fixed;
-	top: 0;
 	z-index: 4;
-	left: 0;
-	right: 0;
 	background-color: #fff;
 	border: 0 solid #ddd;
 	border-bottom-width: 1px;
