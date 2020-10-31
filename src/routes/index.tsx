2df11c8607e4d4109428dcbd5c02eeb8c0eb038d
sk <seonk94@naver.com>
2020-10-31 17:18:15 +0900

    fix: route template set

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import { BrowserRouter, Redirect, Switch } from 'react-router-dom';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+import SignIn from '../template/auth/SignIn';
+import MainTemplate from '../template/main';
 
 function Root() {
 	return (
 		<BrowserRouter>
 			<Switch>
+				<Route exact path="/" component={MainTemplate} />
+				<Route exact path="/login" component={SignIn} />
 				<Redirect path="*" to="/" />
 			</Switch>
 		</BrowserRouter>
