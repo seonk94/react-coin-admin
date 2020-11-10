0a53b1392ed42d661a9fa8f35e347bae120585db
sk <seonk94@naver.com>
2020-11-10 22:29:10 +0900

    fix: Add table header

--- src/components/SymbolTable/index.tsx
+++ src/components/SymbolTable/index.tsx
@@ -14,6 +14,12 @@ function SymbolTable({ coinList, removeCoin, updateCoin }: Props) {
 	return (
 		<TableContainer>
 			<Table>
+				<Table.Header>
+					<Table.Row>
+						<Table.HeaderCell colSpan="3">Coin</Table.HeaderCell>
+						<Table.HeaderCell colSpan="2">Controls</Table.HeaderCell>
+					</Table.Row>
+				</Table.Header>
 				<Table.Body>
 					{coinList.map((data) => (
 						<TableRow data={data} removeCoin={removeCoin} updateCoin={updateCoin} />
