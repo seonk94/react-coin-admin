9281f9482e1576f41d71a2e7e1037e317b4bca98
sk <seonk94@naver.com>
2021-06-11 23:29:39 +0900

    fix: set dotenv

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,6 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 export const PORT = process.env.PORT || 4000;
 export const DB_USER = process.env.DB_USER || '';
 export const DB_NAME = process.env.DB_NAME || '';
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { ApolloServer } from 'apollo-server';
 import clientConnect from './mongo/index.js';
 
-import typeDefs from "./schema.js";
+import typeDefs from './schema.js';
 import resolvers from './resolvers.js';
 
 import { PORT } from './config/index.js';
@@ -9,24 +9,24 @@ import { PORT } from './config/index.js';
 clientConnect();
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  introspection: true,
-  playground: true,
-  subscriptions: {
-    path: '/subscriptions',
-    onConnect: (connectionParams, webSocket, context) => {
-      console.log('Client connected');
-    },
-    onDisconnect: (webSocket, context) => {
-      console.log('Client disconnected');
-    }
-  }
+	typeDefs,
+	resolvers,
+	introspection: true,
+	playground: true,
+	subscriptions: {
+		path: '/subscriptions',
+		onConnect: (connectionParams, webSocket, context) => {
+			console.log('Client connected');
+		},
+		onDisconnect: (webSocket, context) => {
+			console.log('Client disconnected');
+		},
+	},
 });
 
 server.listen({ port: PORT }).then(({ url }) => {
-  console.log(
-    `🚀 Subscription endpoint ready at ws://localhost:${PORT}${server.subscriptionsPath}`
-  );
-  console.log(`🚀 Apollo Server ready at ${url}`);
+	console.log(
+		`🚀 Subscription endpoint ready at ws://localhost:${PORT}${server.subscriptionsPath}`,
+	);
+	console.log(`🚀 Apollo Server ready at ${url}`);
 });
